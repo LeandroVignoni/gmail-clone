30d4bcf44f29505292f38ca297b1da414c020d48
LVignoni <leandrovignoni@gmail.com>
2021-01-26 00:05:15 -0300

    header

---
+++ src/Header.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./Header.css";
+import MenuIcon from '@material-ui/icons/Menu';
+
+function Header() {
+  return( <div className="header" >
+      <div className="header__left"></div>
+      <div className="header__middle"></div>  
+      <div className="header__right"></div>
+      .asd   
+  </div>
+  )
+}
+
+export default Header;
