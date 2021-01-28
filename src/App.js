8195c39eb9232c92d348f5e4fbd6b3ed94bfc98d
LVignoni <leandrovignoni@gmail.com>
2021-01-27 22:23:19 -0300

    27/01 22:30 compose + header

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import './App.css';
 import Header from './Header';
+import Sidebar from './Sidebar';
 
 function App() {
   return (
     <div className="app">
       <Header />
-      <h1>GMAIL CLONE</h1>
+      <Sidebar />
     </div>
   );
 }
--- src/Header.js
+++ src/Header.js
@@ -1,15 +1,42 @@
 import React from "react";
 import "./Header.css";
-import MenuIcon from '@material-ui/icons/Menu';
+import MenuIcon from "@material-ui/icons/Menu";
+import { Avatar, IconButton } from "@material-ui/core";
+import SearchIcon from "@material-ui/icons/Search";
+import ArrowDropDownIcon from "@material-ui/icons/ArrowDropDown";
+import AppsIcon from "@material-ui/icons/Apps";
+import NotificationsIcon from "@material-ui/icons/Notifications";
 
 function Header() {
-  return( <div className="header" >
-      <div className="header__left"></div>
-      <div className="header__middle"></div>  
-      <div className="header__right"></div>
-      .asd   
-  </div>
-  )
+  return (
+    <div className="header">
+      <div className="header__left">
+        <IconButton>
+          <MenuIcon />
+        </IconButton>
+        <img
+          src="https://www.teayudocongoogle.com/wp-content/uploads/2018/11/que-es-gmail-1280x720.png"
+          alt="gmail icon"
+        />
+      </div>
+
+      <div className="header__middle">
+        <SearchIcon />
+        <input placeHolder="Search email" type="text" />
+        <ArrowDropDownIcon className="" />
+      </div>
+
+      <div className="header__right">
+        <IconButton>
+          <AppsIcon />
+        </IconButton>
+        <IconButton>
+          <NotificationsIcon />
+        </IconButton>
+        <Avatar />
+      </div>
+    </div>
+  );
 }
 
 export default Header;
