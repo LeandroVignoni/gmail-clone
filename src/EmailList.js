a2d7300d6dd087f0a63f9885fc96c8ac2dfde024
LVignoni <leandrovignoni@gmail.com>
2021-02-07 23:54:46 -0300

    07/02

--- src/EmailList.js
+++ src/EmailList.js
@@ -3,6 +3,10 @@ import React from "react";
 import ArrowDropDownIcon from "@material-ui/icons/ArrowDropDown";
 import RedoIcon from "@material-ui/icons/Redo";
 import MoreVertIcon from "@material-ui/icons/MoreVert";
+import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
+import ChevronRightIcon from '@material-ui/icons/ChevronRight';
+import KeyboardHideIcon from '@material-ui/icons/KeyboardHide';
+import SettingsIcon from '@material-ui/icons/Settings';
 
 function EmailList() {
   return (
@@ -20,6 +24,23 @@ function EmailList() {
             <MoreVertIcon />
           </IconButton>
         </div>
+        <div className="emailList__settingsRight">
+        <IconButton>
+            <ChevronLeftIcon />
+          </IconButton>
+          <IconButton>
+            <ChevronRightIcon />
+          </IconButton>
+          <IconButton>
+            <KeyboardHideIcon />
+          </IconButton>
+          <IconButton>
+            <SettingsIcon />
+          </IconButton>
+        </div>
+      </div>
+      <div className="emailList__sections">
+        
       </div>
     </div>
   );
