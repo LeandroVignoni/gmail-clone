fd2bfa6a229f0bca2ca6d803003bd20530719663
LVignoni <leandrovignoni@gmail.com>
2021-04-01 13:12:46 -0300

    rendering mails from firebase db

--- src/EmailList.js
+++ src/EmailList.js
@@ -13,8 +13,24 @@ import LocalOfferIcon from "@material-ui/icons/LocalOffer";
 import Section from "./Section";
 import "./EmailList.css";
 import EmailRow from "./EmailRow";
+import { useState } from "react";
+import { db } from "./firebase";
+import { useEffect } from "react";
 
 function EmailList() {
+  const [emails, setEmails] = useState([]);
+  useEffect(() => {
+    db.collection("emails")
+      .orderBy("timestamp", "desc")
+      .onSnapshot((snapshot) =>
+        setEmails(
+          snapshot.docs.map((doc) => ({
+            id: doc.id,
+            data: doc.data(),
+          }))
+        )
+      );
+  }, []);
   return (
     <div className="emailList">
       <div className="emailList__settings">
@@ -46,33 +62,21 @@ function EmailList() {
         </div>
       </div>
       <div className="emailList__sections">
-        <Section
-          Icon={InboxIcon}
-          title="Primary"
-          color="red"
-          selected
-        />
+        <Section Icon={InboxIcon} title="Primary" color="red" selected />
         <Section Icon={PeopleIcon} title="Social" color="#1A73E8" />
-        <Section
-          Icon={LocalOfferIcon}
-          title="Promotions"
-          color="green"
-        />
+        <Section Icon={LocalOfferIcon} title="Promotions" color="green" />
       </div>
       <div className="emailList__list">
-        <EmailRow
-          title="Linkedin"
-          subject="Job offer"
-          description="We are interested in hiring you "
-          time="10pm"
-        />
-        <EmailRow
-          title="Linkedin"
-          subject="Job offer"
-          description="We are interested in hiring you "
-          time="10pm"
-        />
-      
+        {emails.map(({ id, data: { to, subject, message, timestamp } }) => (
+          <EmailRow
+            id={id}
+            key={id}
+            title={to}
+            subject={subject}
+            description={message}
+            time={new Date(timestamp?.seconds * 1000).toUTCString()}
+          />
+        ))}
       </div>
     </div>
   );
--- src/EmailRow.js
+++ src/EmailRow.js
@@ -4,11 +4,25 @@ import StarBorderOutlinedIcon from "@material-ui/icons/StarBorderOutlined";
 import LabelImportantOutlinedIcon from "@material-ui/icons/LabelImportantOutlined";
 import "./EmailRow.css";
 import { useHistory } from "react-router-dom";
-
+import { useDispatch } from "react-redux";
+import { selectMail } from "./features/mailSlice";
 function EmailRow({ title, subject, description, time, id }) {
-    const history = useHistory()    //history of the web page
+  const history = useHistory();
+  const dispatch = useDispatch();
+  const openMail = () => {
+    dispatch(
+      selectMail({
+        id,
+        title,
+        subject,
+        description,
+        time,
+      })
+    );
+    history.push("/mail");
+  };
   return (
-    <div onClick={() => history.push("/mail")}  className="emailRow">
+    <div onClick={openMail} className="emailRow">
       <div className="emailRow__options">
         <Checkbox />
         <IconButton>
@@ -22,9 +36,7 @@ function EmailRow({ title, subject, description, time, id }) {
       <div className="emailRow__message">
         <h4>
           {subject}{" "}
-          <span className="emailRow__description">
-            -{description}
-          </span>
+          <span className="emailRow__description">-{description}</span>
         </h4>
       </div>
       <p className="emailRow__time">{time}</p>
